c5262636619ea6e26dcc7141abe801277516b3eb
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-05-31 21:38:52 +0300

    feat: add more methods and DTOs for cat object

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,21 +1,39 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { CreateCatDto } from './create-cat.dto';
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CreateCatDto, UpdateCatDto, ListAllEntities } from './dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  async create(@Body() createCatDto: CreateCatDto) {
+  create(@Body() createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
 
   @Get()
-  async findAll(@Query('age') age: number, @Query('breed') breed: string) {
-    return `This action returns all cats filtered by age: ${age} and breed: ${breed}`;
+  findAll(@Query() query: ListAllEntities) {
+    return `This action returns all cats (limit: ${query.limit} items)`;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): string {
-    console.log(id);
+  findOne(@Param('id') id: string) {
     return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
   }
 }
